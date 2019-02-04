c12f669c0e087c84d91bfffb0b4f5e771e02111e
Leora Wenger <web@leoraw.com>
2019-02-04 12:26:54 -0500

    started exi app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
+
+const TITLE = 'React GraphQL EXI Client'
+const DEFAULT_QUERY = 'MET'
+
+// const axiosExi = axios.create({
+//   baseURL: 'https://api.iextrading.com/1.0/',
+//   pathSearch: 'tops/last?symbols=',
+//   query: DEFAULT_QUERY
+// })
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            result: null,
+            searchTerm: DEFAULT_QUERY,
+            loading: false
+        }
+    }
+  
+  componentDidMount(){
+      this.setState({loading: true})
+     // axios(axiosExi)
+     //console.log(axiosExi)
+      axios.get(`https://api.iextrading.com/1.0/tops/last?symbols=${DEFAULT_QUERY}`)
+       .then(result => {
+           console.log(result, result.data[0].price, result.data[0].symbol, result.data[0].size)
+    //       this.setState({
+    //             searchTerm: data
+    //           })
+           })
+        .catch(error => console.log(error))   
+  }  
+    
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{TITLE}</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

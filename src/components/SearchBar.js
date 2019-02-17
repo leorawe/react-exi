7bada197ce82d639fc378450e62cb2bf4fa8e726
Leora Wenger <web@leoraw.com>
2019-02-17 15:22:55 -0500

    button back green

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import styled from 'styled-components'
+const GoButton = styled.button`
+    background: #008b00;
+    border-radius: 15px;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border: 1px solid #008b00;
+    color: #fff;
+    margin: 0;
+    padding: 0.5em 1em;
+  `;
+  const GoInput = styled.input`
+    background: white;
+    border: 1px solid #008b00;
+    border-right: none;
+    color: #333;
+    margin: 0;
+    padding: 0.5em 1em;
+  `;
 
 const SearchBar =(props)=> {
     
@@ -11,31 +29,14 @@ const SearchBar =(props)=> {
     //add ui from there??? ui semantics
     // value={this.state.term}
     // onChange={e => this.setState({term: e.target.value})}
- const GoButton = styled.button`
-    background: transparent;
-    border-radius: 15px;
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-    border: 2px solid #008b00;
-    color: #008b00;
-    margin: 0;
-    padding: 0.25em 1em;
-  `;
-//   const GoInput = styled.input`
-//     background: white;
-//     border: 2px solid #008b00;
-//     border-right: none;
-//     color: #333;
-//     margin: 0;
-//     padding: 0.25em 1em;
-//   `;
+ 
     const { value, onChange, onSubmit } = props;
         return (
         <div className="ui segment">
             <form onSubmit={onSubmit} className="ui form">
                 <div className="field">
                 <label>Ticker Symbol Search: </label>
-                <input type="text" placeholder="type"
+                <GoInput type="text" placeholder="type"
                 value={value}
                 onChange={onChange}
                 /><GoButton type="submit">

29ab11677314e1f89ae51bfc64ddaf373b083a65
Leora Wenger <web@leoraw.com>
2019-02-18 10:59:02 -0500

    hide info button

--- src/components/IexInfo.js
+++ src/components/IexInfo.js
@@ -10,26 +10,33 @@ const AboutMore = styled.button`
     border: none;
     color: blue;
     &:focus {
-    outline: none;
-    border-color: none;
-    box-shadow: none;
-         }
+        outline: none;
+        border-color: none;
+        box-shadow: none;
+            }
+    ${'' /* &:after {
+        
+        content: '\\0394';
+        display: inline;
+        text-decoration: none;
+        color: black;
+        }      */}
     }
     `    
 
 const IexInfo = (props) => {
     const {toggle, info } = props;  
     const Teach = () => (
-        <p>Type a ticker symbol into the search box. Ticker symbols are usually three letters, 
+        <p>Type a ticker symbol into the search box. Hit go. Ticker symbols are usually three letters, 
         although some are two. The top information of price, size and when are the last sale on the 
         IEX.  The optional company information below is from a different API, and it might give a description,
-        a company name, the sector or the industry.</p>
+        a company name, the sector or the industry.<AboutMore onClick={toggle}>Hide info</AboutMore></p>
     ) 
   return (
     <About>
       <hr />
         <div>The  Investors Exchange (IEX) is a stock exchange for U.S. equities that is built for investors and companies.
-        <AboutMore onClick={toggle}> Ways use this app.</AboutMore></div>
+        <AboutMore onClick={toggle}> Ways use to this app.</AboutMore></div>
         {!info && <Teach />}
     </About>
   )
